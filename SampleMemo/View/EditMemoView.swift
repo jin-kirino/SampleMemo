07da6431b94eb603d20631527f73ec7eee4fd8a5
jin-kirino <jinkirino@gmail.com>
2023-05-21 16:53:52 +0900

    メモ編集画面/保存機能を実装

--- SampleMemo/View/EditMemoView.swift
+++ SampleMemo/View/EditMemoView.swift
@@ -5,16 +5,57 @@
 //  Created by 神　樹里乃 on 2023/05/21.
 //
 
+
 import SwiftUI
 
 struct EditMemoView: View {
+    // 保存してたメモ内容が格納されている
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var title: String
+    @State private var content: String
+    private var memo: Memo
+    
+    // 初期化：元々入ってたデータを自分に返す
+    init(memo: Memo) {
+        self.memo = memo
+        self.title = memo.title ?? ""
+        self.content = memo.content ?? ""
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("タイトル", text: $title)
+                .textFieldStyle(.roundedBorder)
+                .font(.title)
+            TextEditor(text: $content)
+                .border(Color.gray, width: 1)
+                .font(.body)
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    saveMemo()
+                }) {
+                    Text("保存")
+                }
+            }
+        }
+    }
+    
+    // データベースに保存する
+    private func saveMemo() {
+        memo.title = title
+        memo.content = content
+        memo.updatedAt = Date()
+
+        try? viewContext.save()
     }
 }
 
 struct EditMemoView_Previews: PreviewProvider {
     static var previews: some View {
-        EditMemoView()
+        EditMemoView(memo: Memo())
     }
 }

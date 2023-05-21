cbd7d07054522d22f585ec1432aa2e1290bf8b9f
jin-kirino <jinkirino@gmail.com>
2023-05-21 16:14:35 +0900

    メモ一覧に10個のデータを表示する

--- SampleMemo/ContentView.swift
+++ SampleMemo/ContentView.swift
@@ -9,77 +9,44 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    // 被管理オブジェクトコンテキスト（ManagedObjectContext）の取得
+    // データの生成、保存、取得を行うために必要なオブジェクト
     @Environment(\.managedObjectContext) private var viewContext
 
+    // @FetchRequestで、データベースを検索する
+    // FetchedResults<Memo>型
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        entity: Memo.entity(),
+        sortDescriptors: [NSSortDescriptor(key: "updatedAt", ascending: false)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var fetchedMemoList: FetchedResults<Memo>
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                ForEach(fetchedMemoList) { memo in
+                    VStack {
+                        Text(memo.title ?? "")
+                            .font(.title)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .lineLimit(1)
+                        HStack {
+                            Text(memo.stringUpdatedAt)
+                                .font(.caption)
+                                .lineLimit(1)
+                            Text(memo.content ?? "")
+                                .font(.caption)
+                                .lineLimit(1)
+                            Spacer()
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .navigationTitle("メモ")
+            .navigationBarTitleDisplayMode(.automatic)
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

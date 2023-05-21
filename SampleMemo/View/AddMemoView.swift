c9de77a4c06fdfde247521996794a786afb9d351
jin-kirino <jinkirino@gmail.com>
2023-05-21 16:32:08 +0900

    メモ追加機能を実装

--- SampleMemo/View/AddMemoView.swift
+++ SampleMemo/View/AddMemoView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct AddMemoView: View {
+    // CoreDataのデータが格納されている変数
+    // 
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentation
+    @State private var title: String = ""
+    @State private var content: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("タイトル", text: $title)
+                .textFieldStyle(.roundedBorder)
+                .font(.title)
+            TextEditor(text: $content)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width: 1)
+                .font(.body)
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {addMemo()}) {
+                    Text("保存")
+                }
+            }
+        }
+    }
+    // 保存ボタン押下時の処理
+    private func addMemo() {
+        let memo = Memo(context: viewContext)
+        memo.title = title
+        memo.content = content
+        memo.createdAt = Date()
+        memo.updatedAt = Date()
+        // 生成したインスタンスをCoreDataに保存する
+        try? viewContext.save()
+    
+        presentation.wrappedValue.dismiss()
     }
 }
 

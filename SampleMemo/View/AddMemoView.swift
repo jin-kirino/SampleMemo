ee2a67b87f36d38eb5ac89a5a141f574193186a7
jin-kirino <jinkirino@gmail.com>
2023-05-21 16:31:37 +0900

    Viewファイルを作成し、ContentViewとAddMemoViewを追加

---
+++ SampleMemo/View/AddMemoView.swift
@@ -0,0 +1,20 @@
+//
+//  AddMemoView.swift
+//  SampleMemo
+//
+//  Created by 神　樹里乃 on 2023/05/21.
+//
+
+import SwiftUI
+
+struct AddMemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddMemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddMemoView()
+    }
+}

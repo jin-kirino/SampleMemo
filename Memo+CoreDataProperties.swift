677ac8db1603aad335aa9e3f38157deee16392bd
jin-kirino <jinkirino@gmail.com>
2023-05-21 15:46:15 +0900

    updatedAtの日付をString型で返却

--- Memo+CoreDataProperties.swift
+++ Memo+CoreDataProperties.swift
@@ -24,5 +24,16 @@ extension Memo {
 }
 
 extension Memo : Identifiable {
+    // stringUpdatedAtを呼び出すとString型のupdatedAtが返却される
+        public var stringUpdatedAt: String { dateFomatter(date: updatedAt ?? Date()) }
+        
+        func dateFomatter(date: Date) -> String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy/MM/dd"
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+
+            return dateFormatter.string(from: date)
+        }
 
 }
